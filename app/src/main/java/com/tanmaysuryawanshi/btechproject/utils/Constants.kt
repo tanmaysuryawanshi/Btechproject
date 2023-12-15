6fdc74acd5b46b0d6757f7673773e841dc267e15
Tanmay Suryawanshi <tanmaysuryawanshi123@gmail.com>
2023-12-16 00:39:54 +0530

    ml code added

--- app/src/main/java/com/tanmaysuryawanshi/btechproject/utils/Constants.kt
+++ app/src/main/java/com/tanmaysuryawanshi/btechproject/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.tanmaysuryawanshi.btechproject.utils
 object Constants {
 
     const val Base_Url="https://api.openweathermap.org/"
-
+const val Api_Key=""
     const val COLLECTION_USER="user"
     const val COLLECTION_FARM_DATA="farm"
 }
